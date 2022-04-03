f448cd8fc853d859249bf28150c1a98a26fcafc3
AnierinB <anierin@evolution-x.org>
2022-04-03 11:29:32 +0900

    sm8150-common: Configure & ship DeviceExtras
    Co-authored-by: Master <mhmdeveloperofficial@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -22,7 +22,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-aosp \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -205,6 +206,10 @@ PRODUCT_COPY_FILES += \
 # DeviceSettings
 PRODUCT_PACKAGES += \
     DeviceSettings
+
+# DeviceExtras Package
+PRODUCT_PACKAGES += \
+    DeviceExtras
 
 # Display
 PRODUCT_PACKAGES += \
