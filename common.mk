e479cacccf12c1c1a55f93d8cfac6b68ba79a960
Master <mhmdeveloperofficial@gmail.com>
2023-06-29 11:50:31 +0000

    sm8150-common: Include OnePlus Apps

--- common.mk
+++ common.mk
@@ -259,6 +259,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# OnePlus Apps
+$(call inherit-product-if-exists, vendor/oneplus/apps/apps-vendor.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPAOnePlusSM8150Frameworks \
